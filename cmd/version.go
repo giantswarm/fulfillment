aff67f0f400c29b0f3a696602ca947c9bb9a87f8
Joseph Salisbury <salisbury.joseph@gmail.com>
2024-09-04 15:15:02 +0100

    Adds fulfillment app

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run:   runVersion,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func runVersion(cmd *cobra.Command, args []string) {
+	fmt.Println("0.0.1")
+}
---
+++ handlers/root.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/giantswarm/fulfillment/aws"
+	"github.com/giantswarm/fulfillment/slack"
+)
+
+func Root(w http.ResponseWriter, r *http.Request, c aws.Service, s slack.Service) {
+	switch r.Method {
+	case http.MethodGet:
+		rootGet(w, r)
+	case http.MethodPost:
+		rootPost(w, r, c, s)
+	}
+}
+
+func rootGet(w http.ResponseWriter, r *http.Request) {
+	token := r.URL.Query().Get("token")
+
+	if token == "" {
+		http.Error(w, "Missing or invalid token", http.StatusBadRequest)
+		return
+	}
+
+	escapedToken := url.QueryEscape(token)
+
+	err := Template.Execute(w, map[string]string{"Token": escapedToken})
+	if err != nil {
+		http.Error(w, "Template error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func rootPost(w http.ResponseWriter, r *http.Request, c aws.Service, s slack.Service) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing form: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	email := r.FormValue("email")
+	token := r.FormValue("token")
+
+	unescapedToken, err := url.QueryUnescape(token)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error unescaping token: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	// Note: AWS sends us an opaque token, but it's essentially a base64 string,
+	// and + is a valid base64 character (see https://datatracker.ietf.org/doc/html/rfc4648#section-4),
+	// but browsers will convert it to a space when POSTed as part of a form. So, we hackily convert it back here.
+	unescapedToken = strings.Replace(unescapedToken, " ", "+", -1)
+
+	customerData, err := c.FetchCustomerData(unescapedToken)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error resolving customer: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	customerData.Email = email
+
+	if err := s.PostCustomerData(customerData); err != nil {
+		http.Error(w, fmt.Sprintf("Error posting to Slack: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/success", http.StatusSeeOther)
+}
