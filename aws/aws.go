765a11ec3b1aa0e464892daa71d254f15834e8fc
Joe Salisbury <salisbury.joseph@gmail.com>
2026-01-13 09:18:14 +0000

    Updates AWS SDK (#30)

--- aws/aws.go
+++ aws/aws.go
@@ -7,11 +7,11 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/marketplaceentitlementservice"
 	"github.com/aws/aws-sdk-go-v2/service/marketplacemetering"
-	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/giantswarm/fulfillment/customer"
 )
